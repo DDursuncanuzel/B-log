8ac997a5b85c25374e26f2d54662835bea5d7705
Dursun Can Uzel <dcanuzel@hotmail.com>
2024-08-31 17:07:20 +0300

    footerın eklenmesi

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,12 +3,13 @@ import { Component, NgModule, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
 import { RouterLink } from '@angular/router';
+import { FooterComponent } from "./footer/footer.component";
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, RouterLink ],
+  imports: [RouterOutlet, HeaderComponent, RouterLink, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
